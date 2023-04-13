3c6e7c822139debb3a1f294b33745d179e23787e
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-13 13:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ function getFormvalue() {
 	// Get the values of first name and last name input fields
 	let firstName = document.getElementById("fname").value;
 	let lastName = document.getElementById("lname").value;
-	
+	 
+	// Show an alert with the first and last name
    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
 
-const form = document.getElementById('form1');
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
-  getFormValue(); // Call the getFormValue()
+// Add event listener to form submit
+// const form = document.getElementById('form1');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault(); // Prevent form submission
+//   getFormValue(); // Call the getFormValue()
 	
 
 
